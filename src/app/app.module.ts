b10a2089065e180ca7ea541974b7fd27a7d37723
Szymon-Wiergowski <s.wiergowski@gmail.com>
2021-01-19 10:26:24 +0100

    Create all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserSearchComponent } from './user-search/user-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
+    RepositoriesComponent,
+    UserProfileComponent,
+    UserSearchComponent
   ],
   imports: [
     BrowserModule,
